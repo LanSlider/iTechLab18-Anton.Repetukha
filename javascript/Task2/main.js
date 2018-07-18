b7a02a3841ffa20e98e742bc30b92baeb3328329
LanSlider <dv.lanslider@gmail.com>
2018-07-18 10:20:00 +0300

    add Task2 folder and task1,2,3,4

---
+++ javascript/Task2/main.js
@@ -0,0 +1,29 @@
+var Robot = function (name) {
+	this.name = name;
+}
+
+function add (op1, op2) {
+	this.name = this.name || "Human";
+	return  this.name + " can count to " + (op1 + op2);
+}
+
+var voltron = new Robot("Voltron");
+
+// #1 Выведите результат сложения 0 и 1
+// "Human can count to 1"
+console.log(add(0, 1))
+
+// #2 Выведи результат сложения Voltron 1 и 2 используя call
+// “Voltron can count 3”
+console.log(add.call(voltron, 1, 2))
+
+// #3 Выведи результат сложения Voltron 20 и 30 используя apply
+// “Voltron can count 50”
+console.log(add.apply(voltron, [20, 30]))
+
+// #4 Выведи результат сложения Voltron «drinking» и «beer» используя bind
+// “Voltron can count drinkingbeer”
+console.log(add.bind(voltron)("drinking", "beer"))
+
+
+
