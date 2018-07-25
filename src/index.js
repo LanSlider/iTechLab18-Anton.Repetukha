653c5a7d57b00634a5959cdadc667abe6ffba514
LanSlider <dv.lanslider@gmail.com>
2018-07-25 16:00:31 +0300

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App.js';
-import CounterContainer from './containers/CounterContainer.js';
-
-let index = [<App />,<CounterContainer />];
-
-ReactDOM.render(index, document.getElementById('root'));
