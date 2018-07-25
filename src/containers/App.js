d84615254c0737706899868ab0416d466a8a8369
LanSlider <dv.lanslider@gmail.com>
2018-07-25 15:58:16 +0300

    react counter

---
+++ src/containers/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import AppView from '../views/App/index.js';
+
+class App extends Component {
+  render() {
+    return <AppView />;
+  }
+}
+
+export default App;
---
+++ src/views/App/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from '../../logo.svg';
+import { withStyles } from '@material-ui/core/styles';
+import { styles } from './styles.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
+const AppView = () => {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">React-App</h1>
+          </header>
+          <AppBar className="AppBar" position="static">
+            <Toolbar className="Toolbar">
+              <nav>
+                <ul className="Menu">
+                  <li><a href="#">Counter</a></li>
+                  {/* <li><a href="#">CounterContainer</a></li>
+                  <li><a href="#">Task 3</a></li>   */}
+                </ul>
+              </nav>
+            </Toolbar>
+          </AppBar>     
+        </div>
+)}
+
+export default withStyles(styles)(AppView);
