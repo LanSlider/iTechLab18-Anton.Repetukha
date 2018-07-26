a5d0fc051c255566ccb02232d7dfa787f7c09cd4
LanSlider <dv.lanslider@gmail.com>
2018-07-26 18:11:34 +0300

    edit add index.js

--- react-js/react-app/src/views/App/index.js
+++ react-js/react-app/src/views/App/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from '../../logo.svg';
 import { withStyles } from '@material-ui/core/styles';
 import { styles } from './styles.css';
@@ -7,23 +7,23 @@ import Toolbar from '@material-ui/core/Toolbar';
 
 const AppView = () => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React-App</h1>
-        </header>
-        <AppBar className="AppBar" position="static">
-          <Toolbar className="Toolbar">
-            <nav>
-              <ul className="Menu">
-                <li><a href="#">Counter</a></li>
-                {/* <li><a href="#">CounterContainer</a></li>
-                <li><a href="#">Task 3</a></li>   */}
-              </ul>
-            </nav>
-          </Toolbar>
-        </AppBar>     
-      </div>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">React-App</h1>
+          </header>
+          <AppBar className="AppBar" position="static">
+            <Toolbar className="Toolbar">
+              <nav>
+                <ul className="Menu">
+                  <li><a href="#">Counter</a></li>
+                  {/* <li><a href="#">CounterContainer</a></li>
+                  <li><a href="#">Task 3</a></li>   */}
+                </ul>
+              </nav>
+            </Toolbar>
+          </AppBar>     
+        </div>
 )}
 
 export default withStyles(styles)(AppView);
