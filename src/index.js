8f91e1b224ec47912a89883c1b37b1c20101ca5b
LanSlider <dv.lanslider@gmail.com>
2018-07-27 11:51:06 +0300

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App.js';
-import CounterParent from './containers/CounterParent.js';
-
-let index = [<App />,<CounterParent />];
-
-ReactDOM.render(index, document.getElementById('root'));
