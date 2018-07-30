2e3647279983329a668c56e44a6ef8dd4526d5d2
LanSlider <dv.lanslider@gmail.com>
2018-07-30 15:00:35 +0300

    Updates

--- src/components/Menu/view/styles.js
+++
@@ -1,9 +0,0 @@
-export const styles = () => ({
-    buttonBlock: {
-      "display": "flex",
-      "flex-direction": "row",
-      "align-items": "center",
-      "align-items": "center",
-      "justify-content": "center",
-    },
-  });
