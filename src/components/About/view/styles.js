bee64d3e594c057ce46f2cb4a59b574c8ac071af
LanSlider <dv.lanslider@gmail.com>
2018-07-30 14:52:52 +0300

    add task 3

---
+++ src/components/About/view/styles.js
@@ -0,0 +1,16 @@
+export const styles = () => ({
+    Container: {
+      "padding": "60px", 
+    },
+    ContentContainer: {
+        "min-height": "550px",
+        "padding": "20px"
+    },
+    TextBlock: {
+        "max-width": "830px",
+        "text-align": "center",
+        "margin": "auto",
+        "font-size": "20px",
+        "line-height": "32px"  
+    }
+  });
