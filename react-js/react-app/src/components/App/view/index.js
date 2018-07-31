a0710ba8c62cbfc6b1c12426db9ced845bf124fd
LanSlider <dv.lanslider@gmail.com>
2018-07-31 15:25:02 +0300

    add login form with new tab

--- react-js/react-app/src/components/App/view/index.js
+++ react-js/react-app/src/components/App/view/index.js
@@ -2,6 +2,7 @@ import React from 'react/index.js';
 import Header from '../../Header/container/Header';
 import About from '../../About/view/index';
 import CounterParent from '../../CounterParent/container/CounterParent';
+import Login from '../../Login/container/Login';
 import NotFound from '../../NotFound/view/index';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
@@ -12,6 +13,7 @@ const App = () => {
             <Route exact path="/" component={Header}/>
             <Route exact path="/about" component={About}/>
             <Route exact path="/counter" component={CounterParent}/>
+            <Route exact path="/login" component={Login}/>
             <Route exact path="/404" component={NotFound} />
             <Redirect to="/404" />
         </Switch>
