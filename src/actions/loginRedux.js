36829e7dcf81b706fd7b40f47e8cd10b810f9b9d
LanSlider <dv.lanslider@gmail.com>
2018-08-02 18:19:00 +0300

    Updates

--- src/actions/loginRedux.js
+++
@@ -1,4 +0,0 @@
-import * as types from '../constants/ActionTypes'
-
-export const addEmail = email => ({ type: types.ADD_EMAIL, email: email })
-export const addPassword = password => ({ type: types.ADD_PASSWORD, password: password })
