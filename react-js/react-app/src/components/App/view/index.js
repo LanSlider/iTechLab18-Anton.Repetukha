de703d6e441d7011c322b20c5c56c77253ab266c
LanSlider <dv.lanslider@gmail.com>
2018-08-06 15:35:28 +0300

    edited file name

--- react-js/react-app/src/components/App/view/index.js
+++ react-js/react-app/src/components/App/view/index.js
@@ -4,10 +4,10 @@ import About from '../../About/view';
 import CounterParent from '../../CounterParent/container/CounterParent';
 import Login from '../../Login/container/Login';
 import LoginRedux from '../../LoginRedux/container/LoginRedux';
-import LoginReduxForm from '../../LoginReduxForm/container/LoginReduxForm';
+// import LoginReduxForm from '../../LoginReduxForm/container/LoginReduxForm';
 import NotFound from '../../NotFound/view';
 import LoginSuccess from '../../LoginRedux/container/LoginSuccess';
-import LoginSuccessForm from '../../LoginReduxForm/container/LoginReduxSuccess';
+// import LoginSuccessForm from '../../LoginReduxForm/container/LoginReduxSuccess';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 const App = () => {
@@ -19,10 +19,10 @@ const App = () => {
             <Route exact path="/counter" component={CounterParent}/>
             <Route exact path="/login" component={Login}/>
             <Route exact path="/login-redux" component={LoginRedux}/>
-            <Route exact path="/login-redux-form" component={LoginReduxForm} />
+            {/* <Route exact path="/login-redux-form" component={LoginReduxForm} /> */}
             <Route exact path="/404" component={NotFound} />
             <Route exact path="/login-redux/success" component={LoginSuccess} />
-            <Route exact path="/login-redux-form/success" component={LoginSuccessForm} />
+            {/* <Route exact path="/login-redux-form/success" component={LoginSuccessForm} /> */}
             <Redirect to="/404" />
         </Switch>
     </main>
