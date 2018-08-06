79d3d020104004a47adbcd41150a98e70b6367fe
LanSlider <dv.lanslider@gmail.com>
2018-08-06 10:46:26 +0300

    add new tab login with redux form

--- react-js/react-app/src/components/App/view/index.js
+++ react-js/react-app/src/components/App/view/index.js
@@ -4,10 +4,10 @@ import About from '../../About/view/index';
 import CounterParent from '../../CounterParent/container/CounterParent';
 import Login from '../../Login/container/Login';
 import LoginRedux from '../../LoginRedux/container/LoginRedux';
-// import LoginReduxForm from '../../LoginReduxForm/container/LoginReduxForm';
+import LoginReduxForm from '../../LoginReduxForm/container/LoginReduxForm';
 import NotFound from '../../NotFound/view/index';
 import LoginSuccessView from '../../LoginRedux/viewSuccess/index';
-// import LoginSuccessFormView from '../../LoginReduxForm/viewSuccess/index';
+import LoginSuccessFormView from '../../LoginReduxForm/viewSuccess/index';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 const App = () => {
@@ -19,10 +19,10 @@ const App = () => {
             <Route exact path="/counter" component={CounterParent}/>
             <Route exact path="/login" component={Login}/>
             <Route exact path="/login-redux" component={LoginRedux}/>
-            {/* <Route exact path="/login-redux-form" component={LoginReduxForm} /> */}
+            <Route exact path="/login-redux-form" component={LoginReduxForm} />
             <Route exact path="/404" component={NotFound} />
             <Route exact path="/login-redux/success" component={LoginSuccessView} />
-            {/* <Route exact path="/login-redux-form/success" component={LoginSuccessFormView} /> */}
+            <Route exact path="/login-redux-form/success" component={LoginSuccessFormView} />
             <Redirect to="/404" />
         </Switch>
     </main>
